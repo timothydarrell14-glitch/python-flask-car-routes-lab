a23a740c9ae0188494bd9503fd596bdc461bde8c
TimDev14 <njagi.timothy14@gmail.com>
2026-06-24 14:35:06 +0300

    Implement basic Flask app with routes for car models

--- server/app.py
+++ server/app.py
@@ -1,1 +1,19 @@
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to Flatiron Cars"
+
+@app.route('/<model>')
+def model(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+
